109536efca936edd456c036d2715579d1b7c76a9
ahmedBello14 <ballealarb@gmail.com>
2023-04-16 15:35:38 +0200

    ahmedfathi

---
+++ src1/main/java/com/mycompany/myproject/Myproject.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package com.mycompany.projectteam;
+
+/**
+ *
+ * @author Mega Store
+ */
+
+import java.util.Scanner;
+public class Projectteam {
+  public static void main(String[] args) 
+    {
+        int p, q , z, x , y,k ,change= 0;
+        boolean m=true;
+        Scanner input= new Scanner(System.in);
+        System.out.print("Enter number of rows in matrix:");
+        p = input.nextInt();
+        System.out.print("Enter number of columns in matrix:");
+        q = input.nextInt();
+        int [][]a = new int[p][q];
+        System.out.println("Enter all the elements of matrix:");
+        for (int i = 0; i < p; i++) 
+        {
+            for (int j = 0; j < q; j++) 
+            {
+                a[i][j] = input.nextInt();
+            }
+        }
+        System.out.println("Given Matrix:");
