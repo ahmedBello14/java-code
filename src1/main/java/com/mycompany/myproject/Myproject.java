df6bc85287ce5ef049bf9525699bf80e612f3c55
alaa <alaa.mohamed12345hhh@gmail.com>
2023-04-16 16:26:31 +0200

    Alaa Mohameed Ali

--- src1/main/java/com/mycompany/myproject/Myproject.java
+++ src1/main/java/com/mycompany/myproject/Myproject.java
@@ -11,17 +11,17 @@ package com.mycompany.projectteam;
 
 import java.util.Scanner;
 public class Projectteam {
-  public static void main(String[] args) 
+public static void main(String[] args) 
     {
         int p, q , z, x , y,k ,change= 0;
-        boolean m=true;
+            boolean m=true;
         Scanner input= new Scanner(System.in);
-        System.out.print("Enter number of rows in matrix:");
+            System.out.print("Enter number of rows in matrix:");
         p = input.nextInt();
-        System.out.print("Enter number of columns in matrix:");
+            System.out.print("Enter number of columns in matrix:");
         q = input.nextInt();
         int [][]a = new int[p][q];
-        System.out.println("Enter all the elements of matrix:");
+            System.out.println("Enter all the elements of matrix:");
         for (int i = 0; i < p; i++) 
         {
             for (int j = 0; j < q; j++) 
@@ -29,13 +29,20 @@ public class Projectteam {
                 a[i][j] = input.nextInt();
             }
         }
-        System.out.println("Given Matrix:");
- System.out.println("Given Matrix:");
+            System.out.println("Given Matrix:");
+            System.out.println("Given Matrix:");
         for (int i = 0; i < p; i++) 
-        {
-            for (int j = 0; j < q; j++) 
+      {
+         for (int j = 0; j < q; j++) 
             {
-                System.out.print(a[i][j] + " ");
+            System.out.print(a[i][j] + " ");
             }
             System.out.println("");
-        }
+        }   
+       while (m==true) 
+        {
+            System.out.println("Enter 1 to interchange rows");
+            System.out.println("Enter 2 to interchange columns");
+            System.out.println("Enter 3 to Exit");
+            z=input.nextInt();
+
